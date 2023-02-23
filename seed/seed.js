ae3e6099311c65a69caf0dbe16512f5d9672d1de
Timothy Strom <trss95@outlook.com>
2023-02-23 15:12:13 -0500

    CRUD Achieved

--- seed/seed.js
+++ seed/seed.js
@@ -3,12 +3,20 @@ import 'dotenv/config'
 import Songs from '../models/Songs.js'
 import bookData from './book.json' assert {type: 'json'}
 
+
+
+
+
 //@ts-ignore
 await mongoose.connect(process.env.DATABASE_URL)
+
+
+
 
 await Songs.deleteMany({})
 await Songs.insertMany(bookData)
 
 await mongoose.disconnect()
+
 
 
